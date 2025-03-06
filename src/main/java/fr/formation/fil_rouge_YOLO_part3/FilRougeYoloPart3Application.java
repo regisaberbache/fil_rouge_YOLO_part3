b941afb79830918b1a5edce7d6f2c1072f451252
Maud <maud.gauthier@gmail.com>
2025-03-06 09:18:16 +0100

    update dev

--- src/main/java/fr/formation/fil_rouge_YOLO_part3/FilRougeYoloPart3Application.java
+++ src/main/java/fr/formation/fil_rouge_YOLO_part3/FilRougeYoloPart3Application.java
@@ -3,6 +3,7 @@ package fr.formation.fil_rouge_YOLO_part3;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class FilRougeYoloPart3Application {
 
