8e4589dfb5c7eb9af500dada14b41a1e9c879815
Valentin <valentinhots2@gmail.com>
2025-03-07 12:24:09 +0100

    rest added

---
+++ src/main/java/fr/formation/fil_rouge_YOLO_part3/rest/RestaurantDto/RestaurantDTO.java
@@ -0,0 +1,12 @@
+package fr.formation.fil_rouge_YOLO_part3.rest.RestaurantDto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class RestaurantDTO {
+	private Integer idRestaurantDto;
+}
