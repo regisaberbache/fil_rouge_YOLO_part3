58cb4e4da9403723686e9fc9b4fb4f2c1a78f163
PC_M2I <hffhfhf>
2025-03-12 14:47:17 +0100

    add data rest

--- src/main/java/fr/formation/fil_rouge_YOLO_part3/repository/UtilisateurRepository.java
+++ src/main/java/fr/formation/fil_rouge_YOLO_part3/repository/UtilisateurRepository.java
@@ -1,11 +1,18 @@
 package fr.formation.fil_rouge_YOLO_part3.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import fr.formation.fil_rouge_YOLO_part3.entity.Utilisateur;
 
+//@RepositoryRestResource(collectionResourceRel = "utilisateurs", path = "utilisateur")
 public interface UtilisateurRepository extends JpaRepository<Utilisateur, Integer> {
 
 	Utilisateur findUtilisateurByIdUtilisateur(Integer id);
+	@Query("FROM Utilisateur u where u.role.libelle = :role")
+	Utilisateur trouverUtilisateurParRole(@Param ("role") String role);
+	
+	
 
 }
