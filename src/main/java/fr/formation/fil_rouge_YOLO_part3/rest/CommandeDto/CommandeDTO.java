84fe10a7f1b8e568a39e9011a9fcf67dc6c2682c
PC_M2I <hffhfhf>
2025-03-13 13:09:22 +0100

    adding the CommandeWrapper

--- src/main/java/fr/formation/fil_rouge_YOLO_part3/rest/CommandeDto/CommandeDTO.java
+++ src/main/java/fr/formation/fil_rouge_YOLO_part3/rest/CommandeDto/CommandeDTO.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import fr.formation.fil_rouge_YOLO_part3.entity.Commande;
 import fr.formation.fil_rouge_YOLO_part3.entity.LigneCommande;
+import fr.formation.fil_rouge_YOLO_part3.service.CommandeService;
+import fr.formation.fil_rouge_YOLO_part3.service.CommandeServiceImpl;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -12,21 +14,11 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class CommandeDTO {
+	
 	private Integer idCommande;
 	private String statut;
+	private Integer idReservation;
 	private List<LigneCommande> lignes;
 	
-	public CommandeDTO(Commande commande) {
-		this.idCommande = commande.getIdCommande();
-		this.statut = commande.getStatut();
-		this.lignes = commande.getLignes();		
-	}
-	
-	public Commande toEntity() {
-		Commande commande = new Commande();
-		commande.setIdCommande(idCommande);
-		commande.setStatut(statut);
-		commande.setLignes(lignes);
-		return commande;
-	}
+
 }
