6eaad33913430cd808d2292005f817602f74b723
Maud <maud.gauthier@gmail.com>
2025-03-17 11:33:22 +0100

    commande updated

--- src/main/java/fr/formation/fil_rouge_YOLO_part3/entity/TableRestaurant.java
+++ src/main/java/fr/formation/fil_rouge_YOLO_part3/entity/TableRestaurant.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -12,12 +11,14 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+@Entity
 @Data
 @NoArgsConstructor
-@Entity
+@AllArgsConstructor
 @Table(name = "tables_restaurant")
 public class TableRestaurant {
 	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -38,12 +39,4 @@ public class TableRestaurant {
 	@JoinColumn(name="id_tables_restaurant")
 	private List<Reservation> reservations;
 	
-	public TableRestaurant(Integer nbPlaces, Integer numeroTable, List<Reservation> reservations) {
-		this.nbPlaces = nbPlaces;
-		this.numeroTable = numeroTable;
-		this.reservations = reservations;
-	}
-	
-	
-
 }
