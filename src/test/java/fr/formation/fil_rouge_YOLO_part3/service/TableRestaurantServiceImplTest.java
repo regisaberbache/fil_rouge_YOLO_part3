751409f1bfcdff002f2b4413ed8aa7d9759cfe60
Maud <maud.gauthier@gmail.com>
2025-03-17 14:33:23 +0100

    test in progress

---
+++ src/test/java/fr/formation/fil_rouge_YOLO_part3/service/TableRestaurantServiceImplTest.java
@@ -0,0 +1,14 @@
+package fr.formation.fil_rouge_YOLO_part3.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TableRestaurantServiceImplTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
