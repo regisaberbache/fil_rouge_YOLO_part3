18f071fcdbc3d479651f83beffc9102980e30cbc
CharrVal <charriervalentin2@gmail.com>
2025-04-08 12:35:03 +0200

    fixed tablesNonOccupees

--- src/main/java/fr/formation/fil_rouge_YOLO_part3/rest/ReservationRest.java
+++ src/main/java/fr/formation/fil_rouge_YOLO_part3/rest/ReservationRest.java
@@ -6,6 +6,7 @@ import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,7 +22,7 @@ import fr.formation.fil_rouge_YOLO_part3.service.ReservationService;
 import fr.formation.fil_rouge_YOLO_part3.service.ReservationServiceException;
 import fr.formation.fil_rouge_YOLO_part3.service.RestaurantService;
 import fr.formation.fil_rouge_YOLO_part3.service.RestaurantServiceException;
-
+@CrossOrigin(origins="http://localhost:4200/")
 @RestController
 @RequestMapping("/reservations")
 public class ReservationRest {
--- src/main/java/fr/formation/fil_rouge_YOLO_part3/rest/TableRestaurantRest.java
+++ src/main/java/fr/formation/fil_rouge_YOLO_part3/rest/TableRestaurantRest.java
@@ -28,7 +28,6 @@ import fr.formation.fil_rouge_YOLO_part3.entity.TableRestaurant;
 import fr.formation.fil_rouge_YOLO_part3.rest.DemandeResaDto.DemandeResaDTO;
 import fr.formation.fil_rouge_YOLO_part3.rest.TableRestaurantDto.TableRestaurantDTO;
 import fr.formation.fil_rouge_YOLO_part3.rest.TableRestaurantDto.TableRestaurantLibreDTO;
-import fr.formation.fil_rouge_YOLO_part3.rest.reservationDto.ReservationDTO;
 import fr.formation.fil_rouge_YOLO_part3.service.ReservationService;
 import fr.formation.fil_rouge_YOLO_part3.service.RestaurantService;
 import fr.formation.fil_rouge_YOLO_part3.service.RestaurantServiceException;
@@ -60,31 +59,38 @@ public class TableRestaurantRest {
 	@GetMapping("{idRestau}")
 	public ResponseEntity<List<TableRestaurantDTO>> getTablesNonOccupees(@PathVariable("idRestau") Integer id) {
 		Restaurant restaurant;
-		try {
-			restaurant = restaurantService.getById(id);
-		} catch (RestaurantServiceException e) {
-			return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
-		}
+	    try {
+	        restaurant = restaurantService.getById(id);
+	    } catch (RestaurantServiceException e) {
+	        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+	    }
 
-		List<TableRestaurant> tables = restaurant.getTablesRestaurant();
+	    List<TableRestaurant> tables = restaurant.getTablesRestaurant();
 
-		LocalDateTime debutJournee = LocalDate.now().atStartOfDay();
-		LocalDateTime finJournee = LocalDate.now().atTime(LocalTime.MAX);
+	    LocalDateTime maintenantMoins30 = LocalDateTime.now().minusMinutes(30);
+	    LocalDateTime finJournee = LocalDate.now().atTime(LocalTime.MAX);
 
-		for (TableRestaurant table : tables) {
-			List<Reservation> res = table.getReservations();
-			if (res != null) {
-				table.setReservations(res.stream().filter(r -> {
-					LocalDateTime horaire = r.getHoraireReservation();
-					return horaire != null && !horaire.isBefore(debutJournee) && !horaire.isAfter(finJournee);
-				}).collect(Collectors.toList()));
-			}
-		}
-		List<TableRestaurantDTO> tablesNonOccupees = tables.stream()
-				.map(table -> new TableRestaurantDTO(table))
-				.collect(Collectors.toList());
-		
-		return ResponseEntity.ok(tablesNonOccupees);
+	    for (TableRestaurant table : tables) {
+	        List<Reservation> res = table.getReservations();
+	        if (res != null) {
+	            table.setReservations(
+	                res.stream()
+	                   .filter(r -> {
+	                       LocalDateTime horaire = r.getHoraireReservation();
+	                       return horaire != null
+	                              && !horaire.isBefore(maintenantMoins30)
+	                              && !horaire.isAfter(finJournee);
+	                   })
+	                   .collect(Collectors.toList())
+	            );
+	        }
+	    }
+
+	    List<TableRestaurantDTO> tablesNonOccupees = tables.stream()
+	            .map(table -> new TableRestaurantDTO(table))
+	            .collect(Collectors.toList());
+
+	    return ResponseEntity.ok(tablesNonOccupees);
 	}
 
 	/*
