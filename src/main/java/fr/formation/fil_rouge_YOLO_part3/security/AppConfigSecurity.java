a169e1a24bedcff861c067c6bbf2683511edb281
CharrVal <charriervalentin2@gmail.com>
2025-04-09 17:02:45 +0200

    fixing utilisateurs API

--- src/main/java/fr/formation/fil_rouge_YOLO_part3/security/AppConfigSecurity.java
+++ src/main/java/fr/formation/fil_rouge_YOLO_part3/security/AppConfigSecurity.java
@@ -49,6 +49,7 @@ public class AppConfigSecurity {
 					.requestMatchers(HttpMethod.POST, "/reservations/**").hasAnyAuthority("Admin","Serveur","Chef")
 					.requestMatchers(HttpMethod.POST, "/commandes/**").hasAnyAuthority("Admin","Serveur","Chef")
 					.requestMatchers(HttpMethod.POST, "/tables/**").hasAnyAuthority("Admin","Serveur","Chef")
+					.requestMatchers(HttpMethod.POST, "/utilisateurs/**").hasAnyAuthority("Admin","Serveur","Chef")
 					
 					// Permettre à l'Admin seulement
 					.requestMatchers(HttpMethod.GET, "/utilisateurs").hasAuthority("Admin")
